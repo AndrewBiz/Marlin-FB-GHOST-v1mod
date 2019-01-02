56f68a67ce18e33a50e2d62a64242d8866b8a607
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2019-01-02 20:42:11 +0300

    fix space typos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -617,14 +617,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100,100,400,94 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 94 }
 
  /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300,5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 25 }
 
  /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -632,7 +632,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 1000,1000,200,10000 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 200, 10000 }
 
  /**
  * Default Acceleration (change/s) change = mm/s
