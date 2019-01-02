d35f1e85f4b2f950e930de8458eef0a5981f1bfd
Andrew Bizyaev <andrew.bizyaev@gmail.com>
2019-01-02 23:11:43 +0300

    wath temp increase set to recommended

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -91,7 +91,7 @@
    * below 2.
    */
   #define WATCH_TEMP_PERIOD 60                // Seconds
-  #define WATCH_TEMP_INCREASE 1               // Degrees Celsius
+  #define WATCH_TEMP_INCREASE 3               // Degrees Celsius
 #endif
 
 /**
